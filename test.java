5435562e806f22c5d194186f2dfee8ecb4fa33c2
UltiiimateDog <116751881+UltiiimateDog@users.noreply.github.com>
2023-01-23 11:14:57 -0600

    Add files via upload

--- test.java
+++ test.java
@@ -1,11 +1,59 @@
 package Parcial1;
 
+import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-            Cuadrado test= new Cuadrado();
-            Circulo test2 = new Circulo();
-            System.out.println(test.Diagonal());
-            System.out.println(test2.Area());
+            try (Scanner scan = new Scanner(System.in)) {
+                int select,select2;
+                do {
+                    System.out.println("Seleccione que quiere crear:");
+                    System.out.println("1::Cuadrado");
+                    System.out.println("2::Circulo");
+                    select = scan.nextInt();
+                } while( select < 1 || select > 2 );
+                scan.nextLine();
+                System.out.println("Inserte el ID de su objeto");
+                String ID = scan.nextLine();
+                double id = Double.parseDouble(ID);
+                if (select == 1)    {
+                    Cuadrado object = new Cuadrado(id);
+                    do {
+                        System.out.println("Seleccione que quiere calcular");
+                        System.out.println("1::Perimetro");
+                        System.out.println("2::Area");
+                        System.out.println("3::Diagonal");
+                        select2 = scan.nextInt();
+                    } while (select2 < 1 || select2 > 3);
+                    switch (select2) {
+                        case 1:
+                            System.out.println("Perimetro = " + object.Perimetro());
+                            break;
+                        case 2:
+                            System.out.println("Area = " + object.Area());
+                            break;
+                        case 3:
+                            System.out.println("Diagonal = " + object.Diagonal());
+                            break;
+                    }
+                }
+                else {
+                    Circulo object = new Circulo(id);
+                    do {
+                        System.out.println("Seleccione que quiere calcular");
+                        System.out.println("1::Perimetro");
+                        System.out.println("2::Area");
+                        select2 = scan.nextInt();
+                    } while (select2 < 1 || select2 > 2);
+                    switch (select2) {
+                        case 1:
+                            System.out.println("Perimetro = " + object.Perimetro());
+                            break;
+                        case 2:
+                            System.out.println("Area = " + object.Area());
+                            break;
+                    }
+                }
+            }
     }
 }
